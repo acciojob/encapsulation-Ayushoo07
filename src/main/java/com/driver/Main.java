2dbef88403e0f818632b42029b6ed68a66a5601b
unknown <akhileshayush.2016@gmail.com>
2023-03-14 13:15:35 +0530

    Assignment attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
+public class Main
+{
+    public static void main(String args[])
+    {
+        RWOnly obj=new RWOnly();
+/*
+.\Main.java:7: error: cannot find symbol
+        RWOnly obj=new RWOnly();
+                       ^
+  symbol:   class RWOnly
+  location: class Main
 
-public class Main {
+ */
+        obj.setName("Ayush");
+
+    System.out.println(obj.getName());
+    }
   
 }
